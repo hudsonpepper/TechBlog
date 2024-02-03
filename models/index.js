701089ab2ccaaf5638b1df08dcfb94630d7d1db6
hudsonpepper <75504341+hudsonpepper@users.noreply.github.com>
2024-02-03 08:46:10 -0500

    Seed data made

--- models/index.js
+++ models/index.js
@@ -32,4 +32,4 @@ Comment.belongsTo(Post, {
   foreignKey: 'post_id'
 });
 
-module.exports = { User, Project };
+module.exports = { User, Post, Comment };
--- seeds/seed.js
+++ seeds/seed.js
@@ -1,24 +1,37 @@
 const sequelize = require('../config/connection');
-const { User, Project } = require('../models');
+const { User, Post, Comment } = require('../models');
 
 const userData = require('./userData.json');
-const projectData = require('./projectData.json');
+const postData = require('./postData.json')
+const commentData = require('./commentData.json')
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
-
+  console.log("--------------------- \n DB Setup!! \n--------------------- \n")
   const users = await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
   });
+  console.log("--------------------- \n Users Seeded!! \n--------------------- \n")
 
-  for (const project of projectData) {
-    await Project.create({
-      ...project,
+  let posts = []
+  for (const post of postData) {
+
+    posts.push(await Post.create({
+      ...post,
       user_id: users[Math.floor(Math.random() * users.length)].id,
+    }));
+  }
+  console.log("--------------------- \n Posts Seeded!! \n--------------------- \n")
+
+  for (const comment of commentData) {
+    await Comment.create({
+      ...comment,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+      post_id: posts[Math.floor(Math.random() * posts.length)].id,
     });
   }
-
+  console.log("--------------------- \n Comments Seeded!! \n--------------------- \n")
   process.exit(0);
 };
 
